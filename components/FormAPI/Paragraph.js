20f7049bc6dccc6a131e7eaeea0ee8b58c31d556
Rory <roryd@cleancatalog.com>
2019-06-05 15:29:40 -0700

    paragraph form element

--- components/FormAPI/Paragraph.js
+++ components/FormAPI/Paragraph.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {CheckBox} from "react-native-elements";
+import Textfield from "./Textfield";
 
 export default class Paragraph extends React.Component {
 
@@ -15,10 +16,46 @@ export default class Paragraph extends React.Component {
   render() {
 
     let debugProps = this.props;
-
     let paragraphForm = [];
 
     paragraphForm.push(<Text>Test Paragraph Form</Text>);
+
+    let fields = this.props.field;
+
+    for (const key of Object.keys(fields)) {
+      if (fields.hasOwnProperty(key) && key.charAt(0) !== '#') {
+
+
+        let subfield = fields[key];
+        if(subfield['#type'] === 'container' && subfield['und'] !== undefined) {
+          subfield = subfield['und'][0];
+        }
+
+        if(subfield !== undefined && subfield['#columns'] !== undefined) {
+          paragraphForm.push(<Textfield
+              formValues={this.props.formValues}
+              fieldName={subfield['#field_name']}
+              field={subfield}
+              key={subfield['#field_name']}
+              setFormValue={this.setFormValue}
+          />);
+
+        }
+
+        // We have to sort these into
+
+
+
+
+        let breakpoint;
+
+
+      }
+
+
+    }
+
+
 
     return <View>
       {paragraphForm}
